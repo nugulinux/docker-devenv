a40d14cad88f9c0f0c41a1eefafb6bb6fd957b38
Inho Oh <inho.oh@sk.com>
2019-12-06 15:54:32 +0900

    fix intro message
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- startup.sh
+++ startup.sh
@@ -19,20 +19,22 @@ if [ "$#" -eq 1 ] && [ "$1" = "/bin/zsh" ]; then
 \e[0;37m                                                    \e[0m
 \e[1;30m -------------------------------------------------- \e[0m
 
-Available docker options
- - Share current directory
-   \e[1m-v $PWD:$PWD -w $PWD\e[0m
+  Available docker options
+   - Share current directory
+     \e[1m-v $PWD:$PWD -w $PWD\e[0m
 
- - Audio in docker (PulseAudio)
-   \e[1m-v ~/.config/pulse:/root/.config/pulse \\ \e[0m
-   \e[1m-e PULSE_SERVER={your-ip-address}\e[0m
+   - Audio in docker (PulseAudio)
+     \e[1m-v ~/.config/pulse:/root/.config/pulse \\ \e[0m
+     \e[1m-e PULSE_SERVER={your-ip-address}\e[0m
 
- - NUGU OAuth2 Configurations(http://lvh.me:8080)
-   \e[1m-v ~/nugu:/var/lib/nugu -p 8080:8080\e[0m
+   - NUGU OAuth2 Configurations(http://lvh.me:8080)
+     \e[1m-v ~/nugu:/var/lib/nugu -p 8080:8080\e[0m
 
- - NUGU environment variables
-   \e[1m-e NUGU_OAUTH2_URL=https://api.sktnugu.com/ \\ \e[0m
-   \e[1m-e NUGU_SERVER_TYPE=PRD\e[0m
+  NUGU SDK environment variables
+   - \e[1mNUGU_OAUTH2_URL\e[0m=https://api.sktnugu.com/
+   - \e[1mNUGU_REGISTRY_SERVER\e[0m=https://reg-http.sktnugu.com
+   - \e[1mNUGU_LOG\e[0m=stderr (stderr, syslog, none)
+   - \e[1mNUGU_LOG_MODULE\e[0m=all (default, network, network_trace, audio)
 
 EOF
 )
